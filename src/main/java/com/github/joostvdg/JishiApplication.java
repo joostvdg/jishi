efd6a42fcfeec1f6b33fc5d8501a1fe3b236de49
Joost van der Griendt <j.vandergriendt@flusso.nl>
2017-03-25 18:10:48 +0100

    Initial commit

---
+++ src/main/java/com/github/joostvdg/JishiApplication.java
@@ -0,0 +1,12 @@
+package com.github.joostvdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JishiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JishiApplication.class, args);
+	}
+}
