b2a7c61dca4882ca103e649cd74fb2c53ee9e11f
Joost van der Griendt <j.vandergriendt@flusso.nl>
2017-04-16 13:49:57 +0200

    Remove reliance on vault value

--- src/main/java/com/github/joostvdg/jishi/JishiApplication.java
+++ src/main/java/com/github/joostvdg/jishi/JishiApplication.java
@@ -1,12 +1,9 @@
 package com.github.joostvdg.jishi;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -18,13 +15,5 @@ public class JishiApplication {
 		SpringApplication.run(JishiApplication.class, args);
 	}
 
-    @Value("${password}")
-    String password;
 
-    @PostConstruct
-    private void postConstruct() {
-        System.out.println("---------------------------------------------");
-        System.out.println("My password is: " + password);
-        System.out.println("---------------------------------------------");
-    }
 }
